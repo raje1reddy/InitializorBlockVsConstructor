c079f2db3397b07f395909fd8dce45f049de5970
unknown <raje1reddy@gmail.com>
2019-08-14 18:15:55 +0100

    code updated with two initializers

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,9 +1,15 @@
 
 class Employee {
+
+        {
+                System.out.println("Employee: initializer1"); //initializer block
+        }
+
+
         Employee(){ System.out.println("Employee: constructor");}
 
         {
-                System.out.println("Employee: initializer"); //initializer block
+                System.out.println("Employee: initializer2"); //initializer block
         }
 
 }
